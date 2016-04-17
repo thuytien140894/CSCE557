bf41d098dbf81aaf9f5d27c6fc04274a0cf0a36c
Tien Thuy Ho <thuytien140894@gmail.com>
2016-04-17 12:32:13 -0400

    Solve for the logarithms
    Factor p-1, and compute the logarithms for each factor, then combine
    all the solutions using the Chinese Remainder Theorem

--- Homework_4/index_calculus_discrete_log.py
+++ Homework_4/index_calculus_discrete_log.py
@@ -81,6 +81,20 @@ def multiplicative_inverse(n, p):
 
         return x1
 
+def chinese_remainder(set_of_modulus, set_of_congruences):
+    product = 1
+    sum = 0
+
+    for m in set_of_modulus:
+        product *= m
+
+    for i in range(len(set_of_modulus)):
+        n = product / set_of_modulus[i]
+        inverse = multiplicative_inverse(n, set_of_modulus[i])
+        sum += set_of_congruences[i] * inverse * n
+
+    return sum % product
+
 def gauss_jordan_elimination(matrix):
     ncol = len(matrix[0])
     nrow = len(matrix)
@@ -108,6 +122,7 @@ def gauss_jordan_elimination(matrix):
                         matrix[i][j] = matrix[i][j] - ratio*matrix[k][j]
 
         print_matrix(matrix)
+
     # make the pivot entries all 1s
     return reduce_matrix(matrix)
 
@@ -149,21 +164,28 @@ def solve_for_logarithms(log_relations, prime):
 
 def compute_logarithmic_relations(factor_base, prime, generator):
     log_relations = []
-    limit = len(factor_base) * 2
-    starting_exponent = 1
+    limit = len(factor_base) + 1
+    next_exponent = 1
 
-    log_relations = find_relations(factor_base, prime, generator, log_relations, limit, starting_exponent)
+    # collects initial smooth relations
+    log_relations = find_relations(factor_base, prime, generator,
+                                   log_relations, limit, next_exponent)
+
+    # keeps collecting more relations until m linearly independent relations
+    # are found
     while not is_linearly_independent(log_relations, prime):
-        limit += 2
+        limit += 1
         number_of_relations = len(log_relations)
         number_of_primes = len(factor_base)
-        starting_exponent = log_relations[number_of_relations - 1][number_of_primes] + 1
-        log_relations = find_relations(factor_base, prime, generator, log_relations, limit, starting_exponent)
+        next_exponent = log_relations[number_of_relations - 1][number_of_primes]
+        next_exponent += 1
+        log_relations = find_relations(factor_base, prime, generator,
+                                       log_relations, limit, next_exponent)
 
     return log_relations
 
-def find_relations(factor_base, prime, generator, log_relations, limit, starting_exponent):
-    for i in range(starting_exponent, prime):
+def find_relations(factor_base, prime, generator, log_relations, limit, start):
+    for i in range(start, prime):
         relation = [0] * len(factor_base)
         sieving_value = (generator ** i) % prime
         for j in range(len(factor_base)):
@@ -184,13 +206,16 @@ def find_relations(factor_base, prime, generator, log_relations, limit, starting
 
 def is_linearly_independent(log_relations, prime):
     clone = copy_matrix(log_relations)
-    matrix = finite_field_gauss_elimination(clone, prime)
-    ncol = len(matrix[0])
+    factors = [2, 7043]
 
-    # if one of the pivots is zero, the columns are not linearly independent.
-    for i in range(ncol - 1):
-        if matrix[i][i] == 0:
-            return False
+    for f in factors:
+        matrix = finite_field_gauss_elimination(clone, f)
+        ncol = len(matrix[0])
+
+        # if one of the pivots is zero, the columns are not linearly independent.
+        for i in range(ncol - 1):
+            if matrix[i][i] == 0:
+                return False
 
     return True
 
@@ -204,27 +229,56 @@ def copy_matrix(matrix):
 
     return clone
 
+def find_logarithmic_solutions(log_relations, factor_base):
+    solutions = []
+    # we assume that the factorization is known
+    factors = [2, 7043]
+
+    for f in factors:
+        clone = copy_matrix(log_relations)
+        clone = solve_for_logarithms(clone, f)
+
+        _solution = []
+        for i in range(len(factor_base)):
+            _solution.append(clone[i][len(factor_base)])
+
+        solutions.append(_solution)
+
+    return solutions
+
+# compute final solutions using Chinese Remainder Theorem
+def combine_solutions(solutions):
+    factors = [2, 7043]
+    solution = []
+    for i in range(len(solutions[0])):
+        congruences = []
+        for j in range(len(solutions)):
+            congruences.append(solutions[j][i])
+
+        s = chinese_remainder(factors, congruences)
+        solution.append(s)
+
+    return solution
+
+def compute_individual_log():
+
+    return
+
 def compute_discrete_log(prime, generator, output):
     factor_base = sieve_of_Eratosthenes(15)
-    print factor_base
+    print 'Factor base', factor_base
     log_relations = compute_logarithmic_relations(factor_base, prime, generator)
-    log_relations = solve_for_logarithms(log_relations, prime / 2)
 
-    print_matrix(log_relations)
-    # sample = [[2, 0, 0, 2, 74],
-    #           [7, 1, 0, 0, 64],
-    #           [0, 0, 1, 1, 87],
-    #           [5, 0, 1, 0, 7],
-    #           [1, 1, 2, 0, 360],
-    #           [2, 1, 0, 1, 144],
-    #           [1, 0, 1, 1, 289],
-    #           [0, 2, 1, 0, 313]]
+    # for our purpose, we assume that the factorization of p - 1 is known.
+    # We solve the linear system of these smooth relations separately for each
+    # prime factor and then use the Chinese Remainder Theorem to obtain the
+    # final solution.
 
-    # sample = [[3, 0, 1, 2, 0, 346],
-    #           [6, 0, 0, 1, 1, 171],
-    #           [0, 3, 1, 0, 1, 153],
-    #           [0, 4, 1, 0, 0, 442],
-    #           [3, 5, 1, 0, 0, 458]]
+    # Our prime p is 14087 --> p - 1 = 14086 = 2 * 7043
+    solutions = find_logarithmic_solutions(log_relations, factor_base)
+    final_solution = combine_solutions(solutions)
+
+    print final_solution
 
     # sample = [[1, 1, 0, 0, 0, 0, 0], [1, 0, 0, 1, 1, 0, 0],
     # [1, 1, 0, 1, 0, 0, 0], [0, 1, 0, 0, 1, 0, 0],
@@ -252,7 +306,8 @@ def main():
     #             [0, 1, 0, 0, 1, 0, 2]]
 
     compute_discrete_log(prime, generator, output)
-
+    # x = chinese_remainder([5, 7, 11], [3, 5, 7])
+    # print x
     return
 
 if __name__ == '__main__':
